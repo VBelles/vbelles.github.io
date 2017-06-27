7b855233a1495769fe5a78cf5319701b0abbbc35
VBelles <vicentbelles@gmail.com>
2017-06-27 02:34:56 +0200

    changes...

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-    $("#toggleableContent").find("a").on('click', function(event) {
+    $("#toggleableContent").find("a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - 56
-            }, 300, function(){
+            }, 300, function () {
                 window.location.hash = hash;
             });
         }
@@ -15,20 +15,5 @@ $(document).ready(function () {
     $('.carousel').carousel({
         interval: 3000
     });
-    $('.carousel').on('click', function(event){
-        console.log("wolol");
-        event.preventDefault();
-    });
 
-    // var navOffset = $('.navbar').height();
-    // navOffset = 56;
-    // $('.navbar li a').click(function(event) {
-    //     var href = $(this).attr('href');
-    //     event.preventDefault();
-    //     window.location.hash = href;
-    //
-    //     $(href)[0].scrollIntoView();
-    //     window.scrollBy(0, -navOffset);
-    //
-    // });
 });
